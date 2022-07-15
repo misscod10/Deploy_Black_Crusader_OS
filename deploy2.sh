c1e7fc2dc383748bdf5d16bae13d131d82296e00
Santiago <misscod10@gmail.com>
2022-07-15 19:48:59 +0200

    Test ;)

---
+++ deploy2.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+cp .zshrc ~/.zshrc
+
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
